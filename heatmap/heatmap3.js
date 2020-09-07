d67125576acdc8b5bab85a89bc698ad714fee1c1
Kumirei <48187386+Kumirei@users.noreply.github.com>
2020-09-07 13:36:55 +0200

    Add thousands separator to session count

--- heatmap/heatmap3.js
+++ heatmap/heatmap3.js
@@ -769,7 +769,7 @@
             create_stat_element('Streak', stats.streak[1]+' / '+stats.streak[0], 'Current / Longest'),
         ]})
         let foot_stats = create_elem({type: 'div', class: 'foot-stats stats', children: [
-            create_stat_element('Sessions', stats.sessions, Math.floor(stats.total[0]/stats.sessions)+' per session'),
+            create_stat_element('Sessions', stats.sessions.toSeparated(), Math.floor(stats.total[0]/stats.sessions)+' per session'),
             create_stat_element(type.toProper(), stats.total[0].toSeparated(), create_table("left", [
                 ['This Year', stats.total[1].toSeparated()],
                 ['This Month', stats.total[2].toSeparated()],
