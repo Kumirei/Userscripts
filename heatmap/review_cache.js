7420fdab8b42b6c2b3dba7a115ac2c88e0c53f0b
Kumirei <48187386+Kumirei@users.noreply.github.com>
2020-09-16 23:22:52 +0200

    Refactoring

--- heatmap/review_cache.js
+++ heatmap/review_cache.js
@@ -9,23 +9,14 @@
 // ==/UserScript==
 
 (function(wkof) {
-    if (!wkof) {
-        let script_name = "Review Cache";
-        let response = confirm(script_name + ' requires WaniKani Open Framework.\n Click "OK" to be forwarded to installation instructions.');
-        if (response) window.location.href = 'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549';
-        return;
-    }
     window.review_cache = {
         get_reviews,
         reload,
     }
-    let last_fetch;
 
     function get_reviews() {
         wkof.include('Apiv2');
-        return wkof.ready('Apiv2')
-            .then(load_data)
-            .then(update_data);
+        return wkof.ready('Apiv2').then(load_data).then(update_data);
     }
 
     function reload() {
@@ -33,56 +24,47 @@
     }
 
     function load_data() {
-        return wkof.file_cache.load('review_cache').then(decompress, _=>{return {}});
+        return wkof.file_cache.load('review_cache').then(decompress, _=>{return {date: "1970-01-01T00:00:00.000Z", reviews: [],}});
     }
 
     function save(data) {
-        let compressed = compress(data);
-        return wkof.file_cache.save('review_cache', compressed).then(_=>data);
+        return wkof.file_cache.save('review_cache', compress(data)).then(_=>data);
     }
 
-    function compress(data) {
-        return press(true, data);
-    }
-
-    function decompress(data) {
-        return press(false, data);
-    }
+    function compress(data) {return press(true, data);}
+    function decompress(data) {return press(false, data);}
 
     function press(com, data) {
         let last = 0;
-        return {
-            date: data.date,
-            reviews: data.reviews.map(item => {
+        let pressed = data.reviews.map(item => {
                 let map = [com ? (item[0]-last)/60000 : (last+item[0])*60000, ...item.slice(1)];
                 last = com ? item[0] : last+item[0];
                 return map;
-            })
-        }
+            });
+        return {date: data.date, reviews: pressed};
     }
 
     async function update_data(data) {
         let [date, new_reviews] = await fetch_new_reviews(data.date);
-        if (new_reviews) {
+        if (new_reviews.length) {
             for (let new_review of new_reviews) data.reviews.push(new_review);
             data.reviews.sort((a,b)=>a[0]<b[0]?-1:1);
             data.date = date;
+            save(data);
         }
-        save(data);
         return data.reviews;
     }
 
-    async function fetch_new_reviews(last_fetch=0) {
-        let updated_reviews = await wkof.Apiv2.fetch_endpoint('reviews', {filters: {updated_after: new Date(last_fetch).toISOString()}});
-        if (updated_reviews.total_count == 0) return;
-        updated_reviews.data.sort((a,b)=>Date.parse(a.data.created_at)<Date.parse(b.data.created_at)?-1:1);
-        let new_reviews = updated_reviews.data.filter(item=>last_fetch < Date.parse(item.data.created_at));
-        return [Date.parse(updated_reviews.data_updated_at), new_reviews.map(item=>[Math.floor(Date.parse(item.data.created_at)/60000)*60000,
-                                      item.data.subject_id,
-                                      item.data.starting_srs_stage,
-                                      item.data.incorrect_meaning_answers,
-                                      item.data.incorrect_reading_answers,
-                                  ]),
-                              ];
+    async function fetch_new_reviews(last_fetch) {
+        let updated_reviews = await wkof.Apiv2.fetch_endpoint('reviews', {filters: {updated_after: last_fetch}});
+        let new_reviews = updated_reviews.data.filter(item=> last_fetch<item.data.created_at);
+        new_reviews = new_reviews.map(item=>[
+            Math.floor(Date.parse(item.data.created_at)/60000)*60000,
+            item.data.subject_id,
+            item.data.starting_srs_stage,
+            item.data.incorrect_meaning_answers,
+            item.data.incorrect_reading_answers,
+        ]);
+        return [updated_reviews.data_updated_at, new_reviews];
     }
 })(window.wkof);
