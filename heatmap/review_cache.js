9e28e09f8c69cd60ffa21dbf0ea90cb9955a69dd
Kumirei <48187386+Kumirei@users.noreply.github.com>
2020-09-16 22:40:13 +0200

    Better handling of the last fetch date

--- heatmap/review_cache.js
+++ heatmap/review_cache.js
@@ -29,58 +29,63 @@
     }
 
     function reload() {
-        localStorage.removeItem("review_cache");
         return wkof.file_cache.delete('review_cache').then(get_reviews);
     }
 
     function load_data() {
-        return wkof.file_cache.load('review_cache').then(decompress, _=>[]);
+        return wkof.file_cache.load('review_cache').then(decompress, _=>{return {}});
     }
 
     function save(data) {
-        data = compress(data);
-        return wkof.file_cache.save('review_cache', data).then(_=>{return decompress(data)});
+        let compressed = compress(data);
+        return wkof.file_cache.save('review_cache', compressed).then(_=>data);
     }
 
     function compress(data) {
         let last_time = 0;
-        return data.map(item=>{
-            let map = [(item[0]-last_time)/60000, item[1], item[2], item[3], item[4]];
-            last_time = item[0];
-            return map;
-        });
+        return {
+            date: data.date,
+            reviews: data.reviews.map(item=>{
+                let map = [(item[0]-last_time)/60000, item[1], item[2], item[3], item[4]];
+                last_time = item[0];
+                return map;
+            }),
+        };
     }
 
     function decompress(data) {
         let total_time = 0;
-        return data.map(item=>{
-            total_time += item[0];
-            return [total_time*60000, item[1], item[2], item[3], item[4]];
-        });
+        return {
+            date: data.date,
+            reviews: data.map(item=>{
+                total_time += item[0];
+                return [total_time*60000, item[1], item[2], item[3], item[4]];
+            }),
+        };
     }
 
     async function update_data(data) {
-        let new_reviews = await fetch_new_reviews();
+        let [date, new_reviews] = await fetch_new_reviews(data.date);
         if (new_reviews) {
-            for (let new_review of new_reviews) data.push(new_review);
-            data.sort((a,b)=>a[0]<b[0]?-1:1);
+            for (let new_review of new_reviews) data.reviews.push(new_review);
+            data.reviews.sort((a,b)=>a[0]<b[0]?-1:1);
+            data.date = date;
         }
-        return save(data);
+        save(data);
+        return data.reviews;
     }
 
-    async function fetch_new_reviews() {
-        let last_fetch = localStorage.getItem("review_cache") || "1970-01-01T00:00:00.000Z";
-        let updated_reviews = await wkof.Apiv2.fetch_endpoint('reviews', {filters: {updated_after: last_fetch}});
+    async function fetch_new_reviews(last_fetch=0) {
+        let updated_reviews = await wkof.Apiv2.fetch_endpoint('reviews', {filters: {updated_after: new Date(last_fetch).toISOString()}});
         if (updated_reviews.total_count == 0) return;
-        localStorage.setItem("review_cache", updated_reviews.data_updated_at);
         updated_reviews.data.sort((a,b)=>Date.parse(a.data.created_at)<Date.parse(b.data.created_at)?-1:1);
-        let last_fetch_date = Date.parse(last_fetch);
-        let new_reviews = updated_reviews.data.filter(item=>last_fetch_date < Date.parse(item.data.created_at));
-        return new_reviews.map(item=>[Math.floor(Date.parse(item.data.created_at)/60000)*60000,
+        let new_reviews = updated_reviews.data.filter(item=>last_fetch < Date.parse(item.data.created_at));
+        return [Date.parse(updated_reviews.data_updated_at), new_reviews.map(item=>[Math.floor(Date.parse(item.data.created_at)/60000)*60000,
                                       item.data.subject_id,
                                       item.data.starting_srs_stage,
                                       item.data.incorrect_meaning_answers,
                                       item.data.incorrect_reading_answers,
-                                     ]);
+                                  ]),
+                              ];
     }
 })(window.wkof);
