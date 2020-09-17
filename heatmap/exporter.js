62958f4f9baaf7a38f25226b8db9bed661b14de5
Kumirei <48187386+Kumirei@users.noreply.github.com>
2020-09-17 18:15:05 +0200

    Add initial export test

---
+++ heatmap/exporter.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Wanikani: History Exporter
+// @version      1.0.0
+// @description  Exports items from WK
+// @author       Kumirei
+// ==/UserScript==
+/*jshint esversion: 8 */
+
+(function(wkof) {
+    window.WKExporter = {export_reviews, export_lessons, export_csv};
+
+    async function export_reviews(name, ids) {
+        let reviews = await window.review_cache.get_reviews();
+        let wk_items = wkof.ItemData.get_index(await wkof.ItemData.get_items('assignments'), 'subject_id');
+        let items = [['Date', 'ID', 'SRS start', 'Incorrect meaning answers', 'Incorrect reading answers', 'Item', 'Type']];
+        for (let [time, id, srs, im, ir] of reviews) {
+            if (!ids.includes(id)) continue;
+            items.push([new Date(time).toISOString(), id, srs, im, ir, wk_items[id].data.characters, wk_items[id].object]);
+        }
+        let csv = csvify(items);
+        export_csv(name, csv);
+    }
+
+    async function export_lessons(name, ids) {
+        let lessons = (await wkof.ItemData.get_items('assignments')).filter(item => item.assignments && item.assignments.started_at && ids.includes(item.id));
+        let items = [['Date', 'ID', 'Item', 'Type']];
+        for (let lesson of lessons) {
+            items.push([
+                lesson.assignments.started_at,
+                lesson.id,
+                lesson.data.characters,
+                lesson.object,
+            ]);
+        }
+        let csv = csvify(items);
+        export_csv(name, csv);
+    }
+
+    function csvify(array) {
+        return array.map(row => row.join(', ')).join('\r\n');
+    }
+
+    function export_csv(name, csv) {
+        let encoded = encodeURI("\uFEFF"+csv);
+        let link = document.createElement("a");
+        link.setAttribute("href", 'data:text/csv; charset=utf-8,'+encoded);
+        link.setAttribute("download", name+".csv");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+})(window.wkof);
