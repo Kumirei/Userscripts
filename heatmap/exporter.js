adcf51009695a34f910f8fbac34b9ca5f7388271
Kumirei <48187386+Kumirei@users.noreply.github.com>
2020-09-17 19:30:18 +0200

    Update

--- heatmap/exporter.js
+++ heatmap/exporter.js
@@ -7,22 +7,22 @@
 /*jshint esversion: 8 */
 
 (function(wkof) {
-    window.WKExporter = {export_reviews, export_lessons, export_csv};
+    window.WKExporter = {export_reviews, export_lessons, export_csv, export_items};
+    let reviews, lessons, wk_items;
 
     async function export_reviews(name, ids) {
-        let reviews = await window.review_cache.get_reviews();
-        let wk_items = wkof.ItemData.get_index(await wkof.ItemData.get_items('assignments'), 'subject_id');
+        if (!reviews) reviews = await window.review_cache.get_reviews();
+        if (!wk_items) wk_items = wkof.ItemData.get_index(await wkof.ItemData.get_items('assignments'), 'subject_id');
         let items = [['Date', 'ID', 'SRS start', 'Incorrect meaning answers', 'Incorrect reading answers', 'Item', 'Type']];
         for (let [time, id, srs, im, ir] of reviews) {
             if (!ids.includes(id)) continue;
             items.push([new Date(time).toISOString(), id, srs, im, ir, wk_items[id].data.characters, wk_items[id].object]);
         }
-        let csv = csvify(items);
-        export_csv(name, csv);
+        export_items(items);
     }
 
     async function export_lessons(name, ids) {
-        let lessons = (await wkof.ItemData.get_items('assignments')).filter(item => item.assignments && item.assignments.started_at && ids.includes(item.id));
+        if (!lessons) lessons = (await wkof.ItemData.get_items('assignments')).filter(item => item.assignments && item.assignments.started_at && ids.includes(item.id));
         let items = [['Date', 'ID', 'Item', 'Type']];
         for (let lesson of lessons) {
             items.push([
@@ -32,6 +32,10 @@
                 lesson.object,
             ]);
         }
+        export_items(items);
+    }
+
+    function export_items(items) {
         let csv = csvify(items);
         export_csv(name, csv);
     }
