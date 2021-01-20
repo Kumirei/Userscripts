210a7a02bc383ac47b41852d7263bdc41de50306
Kumirei <Linusson.Sixten@gmail.com>
2021-01-21 00:45:04 +0100

    use HTML tags instead of comments

--- 10chars/userscript.js
+++ 10chars/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani Forums: 10chars
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  Inserts invisible text into any post not meeting the 10 character requirement
 // @author       Kumirei
 // @include      https://community.wanikani.com/t/*
@@ -10,28 +10,33 @@
 /*jshint esversion: 8 */
 
 (function() {
-    // Wait until the save function is defined
-    const i = setInterval(tryInject, 100);
+  // Wait until the save function is defined
+  const i = setInterval(tryInject, 100);
 
-    // Inject if the save function is defined
-    function tryInject() {
-        const old_save = window.require("discourse/controllers/composer").default.prototype.save;
-        if (old_save) {
-            clearInterval(i);
-            inject(old_save);
-        }
+  // Inject if the save function is defined
+  function tryInject() {
+    const old_save = window.require("discourse/controllers/composer").default
+      .prototype.save;
+    if (old_save) {
+      clearInterval(i);
+      inject(old_save);
     }
+  }
 
-    // Wrape the save function with our own function which fills out the post
-    function inject(old_save) {
-        const new_save = function(t){
-            let composer = document.querySelector("textarea.d-editor-input"); // Reply box
-            if (this.model.missingReplyCharacters > 0) {
-                composer.value += ' <!-- Lorem Ipsum -->'; // Modify message
-                composer.dispatchEvent(new Event("change", {bubbles: true, cancelable: true})); // Let Discourse know
-            }
-            old_save.call(this, t); // Call regular save function
-        };
-        window.require("discourse/controllers/composer").default.prototype.save = new_save; // Inject
-    }
+  // Wrape the save function with our own function which fills out the post
+  function inject(old_save) {
+    const new_save = function(t) {
+      let composer = document.querySelector("textarea.d-editor-input"); // Reply box
+      if (this.model.missingReplyCharacters > 0) {
+        composer.value += " <Lorem Ipsum>"; // Modify message
+        composer.dispatchEvent(
+          new Event("change", { bubbles: true, cancelable: true })
+        ); // Let Discourse know
+      }
+      old_save.call(this, t); // Call regular save function
+    };
+    window.require(
+      "discourse/controllers/composer"
+    ).default.prototype.save = new_save; // Inject
+  }
 })();
