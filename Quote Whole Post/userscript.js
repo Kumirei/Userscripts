8ef41ea7a34563691e9e769a829521284a756870
Kumirei <Linusson.Sixten@gmail.com>
2021-02-21 09:55:56 +0100

    move buttons back after change

--- Quote Whole Post/userscript.js
+++ Quote Whole Post/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani Forums: Quote Whole Post
 // @namespace    http://tampermonkey.net/
-// @version      0.1.5
+// @version      0.1.6
 // @description  Adds a button to quote the whole post
 // @author       Kumirei
 // @include      https://community.wanikani.com*
@@ -9,25 +9,34 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    waitForKeyElements('.post-controls .share', function(e){
-        var E = e.closest('article');
-        var btn = document.createElement('button');
-        btn.className = "widget-button btn-flat no-text btn-icon quote-whole-post";
-        btn.title = "Click to quote whole post";
-        btn.innerHTML = '<svg class="fa d-icon d-icon-comment-o svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#far-comment"></use></svg>';
-        btn.onclick = function() {
-            E.find('.widget-button.reply')[0].click();
-            var interval = setInterval(function(){
+;(function () {
+    waitForKeyElements('.post-controls .share', function (e) {
+        var E = e.closest('article')
+        var btn = document.createElement('button')
+        btn.className = 'widget-button btn-flat no-text btn-icon quote-whole-post'
+        btn.title = 'Click to quote whole post'
+        btn.innerHTML =
+            '<svg class="fa d-icon d-icon-comment-o svg-icon svg-string" xmlns="http://www.w3.org/2000/svg"><use xlink:href="#far-comment"></use></svg>'
+        btn.onclick = function () {
+            E.find('.widget-button.reply')[0].click()
+            var interval = setInterval(function () {
                 if ($('button.quote').length) {
-                    $('button.quote').click();
-                    clearInterval(interval);
+                    $('button.quote').click()
+                    clearInterval(interval)
                 }
-            }, 100);
-        };
-        var oldBtn = E.find('button[title="Click to quote whole post"]');
-        if (oldBtn.length) oldBtn.remove();
-        E.find('.post-controls').append(btn);
-    });
-    $('head').append('<style id="QuoteWholePostCSS">.quote-whole-post{float: right; padding: 8px 10px; margin: 1px 0;} .quote-whole-post.d-hover{background-color: rgba(122, 122, 122, 0.17);}</style>');
-})();
+            }, 100)
+        }
+        var oldBtn = E.find('button[title="Click to quote whole post"]')
+        if (oldBtn.length) oldBtn.remove()
+        E.find('.post-controls').append(btn)
+    })
+    $('head').append(
+        '<style id="QuoteWholePostCSS">' +
+            '.quote-whole-post {order: 0; padding: 8px 10px; margin: 1px 0;}' +
+            '.quote-whole-post.d-hover {background-color: rgba(122, 122, 122, 0.17);}' +
+            '.post-controls {justify-content: flex-end !important;}' +
+            '.post-controls .actions {margin: 0 !important; order: 99;}' +
+            '.post-controls .show-replies {position: absolute; left: 0; margin-left: 0px !important;}' +
+            '</style>',
+    )
+})()
