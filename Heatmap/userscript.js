136b648c317b6fdbc2abc5239acc7bc219737041
Kumirei <Linusson.Sixten@gmail.com>
2021-07-20 13:31:18 -0400

    update version number

--- Heatmap/userscript.js
+++ Heatmap/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani Heatmap
 // @namespace    http://tampermonkey.net/
-// @version      3.0.26
+// @version      3.0.27
 // @description  Adds review and lesson heatmaps to the dashboard.
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
