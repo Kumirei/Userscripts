eb3e45323debabd7ad11d80159a4108096d1aded
Kumirei <linusson.sixten@gmail.com>
2021-09-23 12:03:27 -0400

    Update info injector url

--- Stroke Order Diagram/userscript.js
+++ Stroke Order Diagram/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WaniKani Stroke Order
 // @namespace   japanese
-// @version     1.1.10
+// @version     1.1.11
 // @description Shows a kanji's stroke order on its page and during lessons and reviews.
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include     http*://*wanikani.com/kanji/*
@@ -12,7 +12,7 @@
 // @connect     jisho.org
 // @connect     cloudfront.net
 // @require     https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.5.1/snap.svg-min.js
-// @require      https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=969075
+// @require     https://greasyfork.org/scripts/430565-wanikani-item-info-injector/code/WaniKani%20Item%20Info%20Injector.user.js?version=972944
 
 // ==/UserScript==
 
