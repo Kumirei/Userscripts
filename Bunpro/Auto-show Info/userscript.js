222f1f5c83dbfd2b167b6c8103db21384658e98a
Kumirei <linusson.sixten@gmail.com>
2021-11-09 16:03:22 -0500

    possible fix

--- Bunpro/Auto-show Info/userscript.js
+++ Bunpro/Auto-show Info/userscript.js
@@ -1,35 +1,35 @@
 // ==UserScript==
 // @name         Bunpro: Auto-show Info
 // @namespace    http://tampermonkey.net/
-// @version      0.2.8
+// @version      0.2.9
 // @description  Automatically expands the full grammar info when you get a review wrong.
 // @author       Kumirei
 // @include      *bunpro.jp/study*
 // @exclude      *community.bunpro.jp*
+// @require      https://greasyfork.org/scripts/432418-wait-for-selector/code/Wait%20For%20Selector.js?version=974366
 // @require      https://greasyfork.org/scripts/370623-bunpro-helpful-events/code/Bunpro:%20Helpful%20Events.js?version=974369
 // @grant        none
 // ==/UserScript==
 
-(function() {
-	//wait until we're reviewing
-	$('HTML')[0].addEventListener('quiz-page', function() {
-		//do stuff when we press enter and get the answer wrong
-		$('#study-answer-input').on('keydown', function(event) {
-			if (event.which == 13) {
-				openIfWrong();
-			}
-		});
-		//do stuff when we click submit and get the answer wrong
-		$('#submit-study-answer').on('click', function() {
-			openIfWrong();
-		});
+;(function () {
+    //wait until we're reviewing
+    $('HTML')[0].addEventListener('quiz-page', function () {
+        //do stuff when we press enter and get the answer wrong
+        $('#study-answer-input').on('keydown', function (event) {
+            if (event.which == 13) {
+                openIfWrong()
+            }
+        })
+        //do stuff when we click submit and get the answer wrong
+        $('#submit-study-answer').on('click', function () {
+            openIfWrong()
+        })
+    })
 
-	});
-
-	//opens the info if you get the item wrong
-	function openIfWrong() {
-		if ($('.oops-button')[0].style.display == "block" || $('#learn-new-grammar-page').length) {
-			$('.show-grammar-text').click();
-		}
-	}
-})();
+    //opens the info if you get the item wrong
+    function openIfWrong() {
+        if ($('.oops-button')[0].style.display == 'block' || $('#learn-new-grammar-page').length) {
+            $('.show-grammar-text').click()
+        }
+    }
+})()
