6ba456aaf010ac4366845c7d4239c4c669e3a8be
Kumirei <linusson.sixten@gmail.com>
2021-11-20 15:08:41 -0500

    prettier

--- Bunpro/Helpful Events/userscript.js
+++ Bunpro/Helpful Events/userscript.js
@@ -8,44 +8,45 @@
 // @exclude      *community.bunpro.jp*
 // ==/UserScript==
 
-(function (wfs) {
-  // Add a custom event for when BP creates a new body
-  var newBody = new Event("new-body");
-  wfs.wait("body > header", function (e) {
-    fireEvent(newBody);
-  });
+;(function (wfs) {
+    // @require      https://greasyfork.org/scripts/432418-wait-for-selector/code/Wait%20For%20Selector.js?version=974366
+    // Add a custom event for when BP creates a new body
+    var newBody = new Event('new-body')
+    wfs.wait('body > header', function (e) {
+        fireEvent(newBody)
+    })
 
-  // Add a custom event for when you get a new item in reviews
-  var newReviewItem = new Event("new-review-item");
-  wfs.wait(".level_lesson_info a", function (e) {
-    fireEvent(newReviewItem);
-  });
+    // Add a custom event for when you get a new item in reviews
+    var newReviewItem = new Event('new-review-item')
+    wfs.wait('.level_lesson_info a', function (e) {
+        fireEvent(newReviewItem)
+    })
 
-  // Add a custom event when you go to study or cram page
-  var quizPage = new Event("quiz-page");
-  wfs.wait("#show-grammar", function (e) {
-    fireEvent(quizPage);
-  });
+    // Add a custom event when you go to study or cram page
+    var quizPage = new Event('quiz-page')
+    wfs.wait('#show-grammar', function (e) {
+        fireEvent(quizPage)
+    })
 
-  // Add a custom event when you go to study page
-  var studyPage = new Event("study-page");
-  wfs.wait("#study-page #show-grammar", function (e) {
-    fireEvent(studyPage);
-  });
+    // Add a custom event when you go to study page
+    var studyPage = new Event('study-page')
+    wfs.wait('#study-page #show-grammar', function (e) {
+        fireEvent(studyPage)
+    })
 
-  // Add a custom event when you go to cram page
-  var cramPage = new Event("cram-page");
-  wfs.wait("#cram-page #show-grammar", function (e) {
-    fireEvent(cramPage);
-  });
+    // Add a custom event when you go to cram page
+    var cramPage = new Event('cram-page')
+    wfs.wait('#cram-page #show-grammar', function (e) {
+        fireEvent(cramPage)
+    })
 
-  // Fires the given event on the HTML element
-  function fireEvent(event) {
-    var retryInterval = setInterval(function () {
-      if (document.readyState == "complete") {
-        $("HTML")[0].dispatchEvent(event);
-        clearInterval(retryInterval);
-      }
-    }, 100);
-  }
-})(window.wfs);
+    // Fires the given event on the HTML element
+    function fireEvent(event) {
+        var retryInterval = setInterval(function () {
+            if (document.readyState == 'complete') {
+                $('HTML')[0].dispatchEvent(event)
+                clearInterval(retryInterval)
+            }
+        }, 100)
+    }
+})(window.wfs)
