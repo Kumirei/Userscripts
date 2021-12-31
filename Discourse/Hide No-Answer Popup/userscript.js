7e20e612b3d848177645c22bab38ec20df79a724
Kumirei <linusson.sixten@gmail.com>
2021-12-31 15:39:54 -0500

    add hide no-answer popup script

---
+++ Discourse/Hide No-Answer Popup/userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Wanikani Forums: Hide "HAS YOUR QUESTION BEEN ANSWERED" Popup
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hides the most annoying popup that Discourse has ever implemented
+// @author       Kumirei
+// @license MIT
+// @include      *community.wanikani.com*
+// @icon         https://www.google.com/s2/favicons?domain=wanikani.com
+// @grant        none
+// ==/UserScript==
+
+;(function () {
+    document
+        .getElementsByTagName('head')[0]
+        .insertAdjacentHTML('beforeend', `<style id="hide-no-answer-popup">.no-answer {display: none;}</style>`)
+})()
