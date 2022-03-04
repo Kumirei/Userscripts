2abf0f1b02003d46b71fa101d3817bfe50b76e87
Kumirei <linusson.sixten@gmail.com>
2022-03-04 13:09:34 -0500

    fix obstruction of extra study buttons

--- Wanikani/Lessons & Reviews In Header/userscript.js
+++ Wanikani/Lessons & Reviews In Header/userscript.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         Wanikani: Lessons & Reviews in header
 // @namespace    http://tampermonkey.net/
-// @version      1.2.2
+// @version      1.2.3
 // @description  Puts the lessons and review counts back into the header
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
 // @grant        none
 // ==/UserScript==
-/*jshint esversion: 8 */
 
-(function() {
-    document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeEnd', `
+;(function () {
+    document.getElementsByTagName('head')[0].insertAdjacentHTML(
+        'beforeEnd',
+        `
 <style id="L&RHeader">
     .navigation-shortcuts.hidden {display: flex; visibility: visible;}
     .lessons-and-reviews {display: none;}
-    .dashboard-progress {grid-row: 1/3 !important;}
-</style>`);
-})();
+    .extra-study {grid-row: 1/3 !important;}
+</style>`,
+    )
+})()
