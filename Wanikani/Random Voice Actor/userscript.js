62ad7ef835ae1e509237b76c7703681a840cfa76
Kumirei <linusson.sixten@gmail.com>
2022-03-05 16:46:09 -0500

    add to extra study page

--- Wanikani/Random Voice Actor/userscript.js
+++ Wanikani/Random Voice Actor/userscript.js
@@ -1,26 +1,23 @@
 // ==UserScript==
 // @name        Wanikani: Random voice actor
-// @description Randomises the preferred voice actor
-// @include     *wanikani.com/review/session*
-// @include     *wanikani.com/lesson/session*
-// @version     1.1.2
+// @description Randomizes the preferred voice actor
+// @include     /^https://(www|preview).wanikani.com/(lesson|review|extra_study)/session/
+// @version     1.2.0
 // @author      Kumirei
 // @license     MIT; http://opensource.org/licenses/MIT
 // @run-at      document-end
 // @grant       none
-// @namespace https://greasyfork.org/users/105717
+// @namespace   https://greasyfork.org/users/105717
 // ==/UserScript==
-/*jshint esversion: 8 */
 
-(function() {
-	let rand = Math.random;
-	$.jStorage.listenKeyChange('currentItem', randomize_voice_actor);
-	$.jStorage.listenKeyChange('l/currentQuizItem', randomize_voice_actor);
-	$.jStorage.listenKeyChange('l/currentLesson', randomize_voice_actor);
+;(function () {
+    $.jStorage.listenKeyChange('currentItem', randomize_voice_actor)
+    $.jStorage.listenKeyChange('l/currentQuizItem', randomize_voice_actor)
+    $.jStorage.listenKeyChange('l/currentLesson', randomize_voice_actor)
 
-	function randomize_voice_actor() {
-		let r = rand();
-		let voice_actor = (r > 0.5 ? 2 : 1);
-		WaniKani.default_voice_actor_id = voice_actor;
-	}
-})();
+    function randomize_voice_actor() {
+        let r = Math.random()
+        let voice_actor = r > 0.5 ? 2 : 1
+        WaniKani.default_voice_actor_id = voice_actor
+    }
+})()
