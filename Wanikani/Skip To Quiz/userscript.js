efbaed6d3f5680bc2de01bc3c5010ecc5f881475
Kumirei <linusson.sixten@gmail.com>
2022-03-25 12:08:37 -0400

    fix for multiple lesson batches

--- Wanikani/Skip To Quiz/userscript.js
+++ Wanikani/Skip To Quiz/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani: Skip to Quiz
 // @namespace    http://tampermonkey.net/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Enables the quiz button without having to go through any of the lessons.
 // @author       Kumirei
 // @match        https://www.wanikani.com/lesson/session
@@ -10,29 +10,37 @@
 // ==/UserScript==
 
 ;(function ($) {
-    // Wait until button is ready
-    const interval = setInterval(() => {
-        if (!$('#lesson > div:last-child ul > li:last-child button').length) return
-        clearInterval(interval)
-        run()
-    }, 100)
+    // Run on page load
+    run()
+
+    // Also run whenever a new batch of lessons is loaded
+    $.jStorage.listenKeyChange('l/quizActive', run)
+
+    // Add hotkey
+    $('body').on('keydown', (e) => {
+        if (e.key === 'q' && !$.jStorage.get('l/quizActive')) {
+            e.preventDefault()
+            go_to_quiz()
+        }
+    })
 
     function run() {
-        const quizButton = $('#lesson > div:last-child ul > li:last-child button')
+        // Wait until button is ready
+        const interval = setInterval(() => {
+            if ($.jStorage.get('l/quizActive', false)) return
+            const quizButton = $('#lesson > div:last-child ul > li:last-child button')
+            if (!quizButton.length) return
+            clearInterval(interval)
+            make_button_clickable(quizButton)
+        }, 100)
+    }
 
+    function make_button_clickable(quizButton) {
         // Make quiz button look clickable
         quizButton.removeAttr('disabled').addClass('animate-pulse bg-green-400 wk-shadow')
 
         // Go to quiz when button is clicked
         quizButton.on('click', go_to_quiz)
-
-        // Add hotkey
-        $('body').on('keydown', (e) => {
-            if (e.key === 'q' && !$.jStorage.get('l/quizActive')) {
-                e.preventDefault()
-                go_to_quiz()
-            }
-        })
     }
 
     function go_to_quiz() {
