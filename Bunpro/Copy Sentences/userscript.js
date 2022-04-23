4c4bcfc785269a12cdef3e2b43349555f47ed8a5
Kumirei <linusson.sixten@gmail.com>
2022-04-23 12:51:54 -0400

    fix BP copy sentences buttons

--- Bunpro/Copy Sentences/userscript.js
+++ Bunpro/Copy Sentences/userscript.js
@@ -1,74 +1,69 @@
 // ==UserScript==
 // @name         Bunpro: Copy Sentences
 // @namespace    http://tampermonkey.net/
-// @version      0.4.11
+// @version      0.4.12
 // @description  Adds buttons to copy the Japanese and English sentences in review.
 // @author       Kumirei
 // @include      *bunpro.jp/*
 // @exclude      *community.bunpro.jp*
 // @require      https://greasyfork.org/scripts/370623-bunpro-helpful-events/code/Bunpro:%20Helpful%20Events.js?version=974369
-// @require      https://greasyfork.org/scripts/370219-bunpro-buttons-bar/code/Bunpro:%20Buttons%20Bar.js?version=654288
+// @require      https://greasyfork.org/scripts/370219-bunpro-buttons-bar/code/Bunpro:%20Buttons%20Bar.js?version=981624
 // @grant        none
 // ==/UserScript==
 
-(function() {
+;(function () {
     //add buttons
-    $('HTML')[0].addEventListener('quiz-page', function() {
-        buttonsBar.addButton('copyJP', 'Copy JP', 'copyText(parseSentence($(\'.study-question-japanese > div\')[0]));');
-        buttonsBar.addButton('copyEN', 'Copy EN', 'copyText($(\'.study-question-english-hint > span\')[0].innerText);');
-    });
-})();
+    $('HTML')[0].addEventListener('quiz-page', function () {
+        if (!$('#copyJP').length)
+            buttonsBar.addButton('copyJP', 'Copy JP', () =>
+                copyText(parseSentence($('.study-question-japanese > div')[0])),
+            )
+        if (!$('#copyEN').length)
+            buttonsBar.addButton('copyEN', 'Copy EN', () =>
+                copyText($('.study-question-english-hint > span')[0].textContent),
+            )
+    })
 
-//Extracts the sentence from the sentence element
-parseSentence = function(sentenceElem) {
-    var sentence = "";
-    sentenceElem.childNodes.forEach(function(elem) {
-        // find the text in each kind of element and append it to the sentence string
-        var name = elem.nodeName;
-        if (name == "#text") {
-            sentence += elem.data;
-        }
-        else if (name == "STRONG" && elem.children.length) {
-            sentence += elem.children[0].childNodes[0].data;       // with kanji in url
-            //sentence += elem.children[0].children[1].innerText;     // with kana in url
-        }
-        else if (name == "SPAN" && ["chui", "study-area-input"].includes(elem.className)) {
-            if (elem.innerText == "") sentence += "____";
-            else sentence += elem.innerText;
-        }
-        else if (name == "RUBY") {
-            sentence += elem.childNodes[0].data;       // with kanji in string
-            //sentence += elem.children[1].innerText;     // with kana in string
-        }
-    });
-    return sentence;
-};
+    //Extracts the sentence from the sentence element
+    function parseSentence(sentenceElem) {
+        var sentence = ''
 
-//copies the text
-copyText = function(text) {
-    var textArea = document.createElement("textarea");
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-    textArea.style.padding = 0;
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-    textArea.style.background = 'transparent';
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copying text command was ' + msg);
-        console.log('Copied Sentence:', text);
-    } catch (err) {
-        console.log('Oops, unable to copy');
+        var list = sentenceElem.childNodes
+
+        list.forEach(function (currentValue, currentIndex, listObj) {
+            var elem = currentValue
+            var name = currentValue.tagName
+            var className = currentValue.className
+
+            if (name == 'SPAN') {
+                if (['study-area-input'].includes(className)) {
+                    sentence += '____'
+                } else if (['vocab-popout', 'gp-popout'].includes(className)) {
+                    const items = elem.childNodes
+                    items.forEach(function (item) {
+                        if (item.tagName == 'RUBY') {
+                            sentence += item.childNodes[0].data
+                        } else {
+                            sentence += item.textContent
+                        }
+                    })
+                }
+            } else if (name == 'RUBY') {
+                sentence += elem.childNodes[0].data
+            } else {
+                if (elem instanceof HTMLElement) {
+                    sentence += elem.textContent
+                } else if (elem instanceof Text) {
+                    sentence += elem.textContent
+                }
+            }
+        })
+
+        return sentence
     }
 
-    document.body.removeChild(textArea);
-};
+    //copies the text
+    copyText = function (text) {
+        navigator.clipboard.writeText(text)
+    }
+})()
