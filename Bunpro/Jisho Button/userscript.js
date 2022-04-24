f9d2cfa42c511d589cf0767bb316055e1a1a3074
Kumirei <linusson.sixten@gmail.com>
2022-04-24 11:55:08 -0400

    update buttons bar library

--- Bunpro/Jisho Button/userscript.js
+++ Bunpro/Jisho Button/userscript.js
@@ -1,52 +1,68 @@
 // ==UserScript==
 // @name         Bunpro: Jisho Button
 // @namespace    http://tampermonkey.net/
-// @version      0.5.10
+// @version      0.5.11
 // @description  Searches the sentence on Jisho.
 // @author       Kumirei
 // @include      *bunpro.jp/*
 // @exclude      *community.bunpro.jp*
 // @require      https://greasyfork.org/scripts/370623-bunpro-helpful-events/code/Bunpro:%20Helpful%20Events.js?version=974369
-// @require      https://greasyfork.org/scripts/370219-bunpro-buttons-bar/code/Bunpro:%20Buttons%20Bar.js?version=654288
+// @require      https://greasyfork.org/scripts/370219-bunpro-buttons-bar/code/Bunpro:%20Buttons%20Bar.js?version=1043642
 // @grant        none
 // ==/UserScript==
 
-(function() {
+;(function () {
     //Wait until we're on the study page and can add the button
-    $('HTML')[0].addEventListener('quiz-page', function() {
+    $('HTML')[0].addEventListener('quiz-page', function () {
         //add button
-        buttonsBar.addButton('JishoButton', 'Jisho', 'window.open(\'https://www.jisho.org/search/\' + parseSentence($(\'.study-question-japanese > div\')[0]))');
+        if (!$('#JishoButton').length)
+            buttonsBar.addButton('JishoButton', 'Jisho', () =>
+                window.open(`https://www.jisho.org/search/${parseSentence($('.study-question-japanese > div')[0])}`),
+            )
 
         //Bind J to the Jisho button
-        $('#study-answer-input').on('keyup', function(e) {
-            if (e.which == 74 && $('#submit-study-answer').attr('value') == "Next") {
-                $('#JishoButton').click();
+        $('#study-answer-input').on('keyup', function (e) {
+            if (e.which == 74 && $('#submit-study-answer').attr('value') == 'Next') {
+                $('#JishoButton').click()
             }
-        });
-    });
-})();
+        })
+    })
 
-//Extracts the sentence from the sentence elements
-parseSentence = function(sentenceElem) {
-    var sentence = "";
-    sentenceElem.childNodes.forEach(function(elem) {
-        // find the text in each kind of element and append it to the sentence string
-        var name = elem.nodeName;
-        if (name == "#text") {
-            sentence += elem.data;
-        }
-        else if (name == "STRONG" && elem.children.length) {
-            sentence += elem.children[0].childNodes[0].data;       // with kanji in url
-            //sentence += elem.children[0].children[1].innerText;     // with kana in url
-        }
-        else if (name == "SPAN" && elem.className == "study-area-input") {
-            if (elem.innerText == "") sentence += "____";
-            else sentence += elem.innerText;
-        }
-        else if (name == "RUBY") {
-            sentence += elem.childNodes[0].data;       // with kanji in url
-            //sentence += elem.children[1].innerText;     // with kana in url
-        }
-    });
-    return sentence;
-};
+    //Extracts the sentence from the sentence elements
+    function parseSentence(sentenceElem) {
+        var sentence = ''
+
+        var list = sentenceElem.childNodes
+
+        list.forEach(function (currentValue, currentIndex, listObj) {
+            var elem = currentValue
+            var name = currentValue.tagName
+            var className = currentValue.className
+
+            if (name == 'SPAN') {
+                if (['study-area-input'].includes(className)) {
+                    sentence += '____'
+                } else if (['vocab-popout', 'gp-popout'].includes(className)) {
+                    const items = elem.childNodes
+                    items.forEach(function (item) {
+                        if (item.tagName == 'RUBY') {
+                            sentence += item.childNodes[0].data
+                        } else {
+                            sentence += item.textContent
+                        }
+                    })
+                }
+            } else if (name == 'RUBY') {
+                sentence += elem.childNodes[0].data
+            } else {
+                if (elem instanceof HTMLElement) {
+                    sentence += elem.textContent
+                } else if (elem instanceof Text) {
+                    sentence += elem.textContent
+                }
+            }
+        })
+
+        return sentence
+    }
+})()
