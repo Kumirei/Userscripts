055f5bfb205fbc3656edc454317b13d7e4f89e8f
Kumirei <linusson.sixten@gmail.com>
2022-05-03 13:47:44 -0400

    Heatmap: Update depedency

--- Wanikani/Heatmap/userscript.js
+++ Wanikani/Heatmap/userscript.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Wanikani Heatmap
 // @namespace    http://tampermonkey.net/
-// @version      3.0.48
+// @version      3.0.49
 // @description  Adds review and lesson heatmaps to the dashboard.
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
 // @require      https://greasyfork.org/scripts/410909-wanikani-review-cache/code/Wanikani:%20Review%20Cache.js?version=1046768
-// @require      https://greasyfork.org/scripts/410910-heatmap/code/Heatmap.js?version=888244
+// @require      https://greasyfork.org/scripts/410910-heatmap/code/Heatmap.js?version=1046781
 // @grant        none
 // ==/UserScript==
 
