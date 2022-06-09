354bf72c5a21bf8512f08e4adfb241f604033805
Kumirei <linusson.sixten@gmail.com>
2022-06-09 18:10:01 -0400

    B2B: Add behavior setting

--- Wanikani/Back To Back/userscript.js
+++ Wanikani/Back To Back/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani: Back to back
 // @namespace    http://tampermonkey.net/
-// @version      1.2.7
+// @version      1.3.0
 // @description  Makes reading and meaning appear back to back in reviews and lessons
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(lesson|review|extra_study)/session/
@@ -11,29 +11,26 @@
 
 ;(function (wkof, $) {
     // Page related info
-    let currentItemKey, questionTypeKey, activeQueueKey, UIDPrefix, traceFunctionName
+    let current_item_key, question_type_key, active_queue_key, UID_prefix
     let REVIEWS, LESSONS, EXTRA_STUDY
     if (/REVIEW/i.test(location.pathname)) {
         REVIEWS = true
-        currentItemKey = 'currentItem'
-        questionTypeKey = 'questionType'
-        activeQueueKey = 'activeQueue'
-        UIDPrefix = ''
-        traceFunctionName = /randomQuestion/
+        current_item_key = 'currentItem'
+        question_type_key = 'questionType'
+        active_queue_key = 'activeQueue'
+        UID_prefix = ''
     } else if (/LESSON/i.test(location.pathname)) {
         LESSONS = true
-        currentItemKey = 'l/currentQuizItem'
-        questionTypeKey = 'l/questionType'
-        activeQueueKey = 'l/activeQueue'
-        UIDPrefix = 'l/stats/'
-        traceFunctionName = /selectItem/
+        current_item_key = 'l/currentQuizItem'
+        question_type_key = 'l/questionType'
+        active_queue_key = 'l/activeQueue'
+        UID_prefix = 'l/stats/'
     } else if (/EXTRA_STUDY/i.test(location.pathname)) {
         EXTRA_STUDY = true
-        currentItemKey = 'currentItem'
-        questionTypeKey = 'questionType'
-        activeQueueKey = 'practiceQueue'
-        UIDPrefix = 'e/stats/'
-        traceFunctionName = /selectQuestion/
+        current_item_key = 'currentItem'
+        question_type_key = 'questionType'
+        active_queue_key = 'practiceQueue'
+        UID_prefix = 'e/stats/'
     }
 
     // Script info
@@ -58,30 +55,57 @@
 
     // Set up back to back meaning/reading reviews
     function install_back2back() {
+        const settings = wkof.settings[script_id]
+
+        // Keep track of the latest answer to decide whether to show the next question right away
+        let last_answer = false
+
         // Wrap jStorage.set(key, value) to ignore the value when the key is for the current item AND one item has
         // already been partially answered. If an item has been partially answered, then set the current item to
         // that item instead.
         const original_set = $.jStorage.set
         const new_set = function (key, value, options) {
-            if (key === currentItemKey && wkof.settings[script_id].active) {
-                const active_queue = $.jStorage.get(activeQueueKey, [])
-                for (const item of active_queue) {
-                    const UID = (item.type == 'Kanji' ? 'k' : 'v') + item.id
-                    const stats = $.jStorage.get(UIDPrefix + UID)
-                    // Change the item if it has been answered in the session, regardless of whether the answer
-                    // was correct.
-                    if (stats) {
-                        if (stats.mc) $.jStorage.set(questionTypeKey, 'reading')
-                        if (stats.rc) $.jStorage.set(questionTypeKey, 'meaning')
-                        // Set active queue such that the new current item is at the front
-                        const new_active_queue = [item, ...active_queue.filter((i) => i !== item)]
-                        $.jStorage.set(activeQueueKey, new_active_queue)
-                        // Set the item
-                        return original_set.call(this, key, item, options)
-                    }
+            const pass = (val) => original_set.call(this, key, val, options)
+
+            // If an answer is being registered
+            if (RegExp(`^${UID_prefix}[rkv]\\d+$`).test(key)) {
+                const prev = { mc: 0, rc: 0, mi: 0, ri: 0, ...$.jStorage.get(key, {}) }
+                const curr = { mc: 0, rc: 0, mi: 0, ri: 0, ...value }
+
+                if (prev.mc < curr.mc || prev.rc < curr.rc) last_answer = true
+                else if (prev.mi < curr.mi || prev.ri < curr.ri) {
+                    last_answer = false
+                    // If the script is set to always show both answers, remove any correct answers already registered
+                    if (settings.behavior === 'true') return pass({ ...curr, mc: undefined, rc: undefined })
                 }
             }
-            return original_set.call(this, key, value, options)
+            // If the current item is being set
+            else if (key === current_item_key && settings.active) {
+                let item = $.jStorage.get(current_item_key)
+                const active_queue = $.jStorage.get(active_queue_key, [])
+                if (!item) return pass(value)
+                if (settings.behavior !== 'always' && !last_answer) return pass(value)
+                // ! Potential issue when reordering and the current item is still in the active queue
+                // ! If behavior is 'always' or last answer was correct, the current item will stay the current item
+                // Find the item in the active queue. If it is not there, pass
+                item = active_queue.find((i) => i.id === item.id)
+                if (!item) return pass(value)
+
+                // Bring the item to the front of the active queue
+                const new_active_queue = [item, ...active_queue.filter((i) => i !== item)]
+                $.jStorage.set(active_queue_key, new_active_queue)
+                // Set the question type
+                let question = 'meaning'
+                if (item.type !== 'Radical') {
+                    const UID = (item.type == 'Kanji' ? 'k' : 'v') + item.id
+                    const stats = $.jStorage.get(UID_prefix + UID, {})
+                    question = ['meaning', 'reading'][stats.mc ? 1 : stats.rc ? 0 : Math.floor(Math.random() * 2)]
+                }
+                $.jStorage.set(question_type_key, question)
+                // Pass the value to the original set function
+                return pass(item)
+            } // @ts-ignore
+            return pass(value)
         }
         $.jStorage.set = new_set
     }
@@ -89,7 +113,7 @@
     // Set up prioritization of reading or meaning
     function install_prioritization() {
         // Run every time item changes
-        $.jStorage.listenKeyChange(currentItemKey, prioritize)
+        $.jStorage.listenKeyChange(current_item_key, prioritize)
         // Initialize session to prioritized question type
         prioritize()
     }
@@ -97,16 +121,16 @@
     // Prioritize reading or meaning
     function prioritize() {
         const prio = wkof.settings[script_id].prioritize
-        const item = $.jStorage.get(currentItemKey)
+        const item = $.jStorage.get(current_item_key)
         // Skip if item is a radical, it is already the right question, or no priority is selected
-        if (item.type == 'Radical' || $.jStorage.get(questionTypeKey) == prio || 'none' == prio) return
+        if (item.type == 'Radical' || $.jStorage.get(question_type_key) == prio || 'none' == prio) return
         const UID = (item.type == 'Kanji' ? 'k' : 'v') + item.id
-        const done = $.jStorage.get(UIDPrefix + UID)
+        const done = $.jStorage.get(UID_prefix + UID)
         // Change the question if no question has been answered yet,
         // Or the priority question has not been answered correctly yet
         if (!done || !done[prio == 'reading' ? 'rc' : 'mc']) {
-            $.jStorage.set(questionTypeKey, prio)
-            $.jStorage.set(currentItemKey, item)
+            $.jStorage.set(question_type_key, prio)
+            $.jStorage.set(current_item_key, item)
         }
     }
 
@@ -132,6 +156,7 @@
     function load_settings() {
         const defaults = {
             prioritize: 'none',
+            behavior: 'always',
             active: true,
         }
         return wkof.Settings.load(script_id, defaults)
@@ -156,6 +181,18 @@
             on_save: prioritize,
             content: {
                 active: { type: 'checkbox', label: 'Active', default: true },
+                behavior: {
+                    type: 'dropdown',
+                    default: 'always',
+                    label: 'Behavior',
+                    hover_tip:
+                        'Choose whether to:\n1. Keep repeating the same question until you get it right\n2. Only keep the item if you answered the first question correctly\n3. Make it so that you have to answer both questions correctly back to back',
+                    content: {
+                        always: 'Repeat until correct',
+                        correct: 'Shuffle incorrect',
+                        true: 'True Back To Back',
+                    },
+                },
                 prioritize: {
                     type: 'dropdown',
                     label: 'Prioritize',
