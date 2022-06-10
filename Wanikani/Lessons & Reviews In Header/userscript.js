4b7721ee829b6b581decc24d2791cc8f4cef9e46
Kumirei <linusson.sixten@gmail.com>
2022-06-10 14:10:29 -0400

    Update Lessons and Reviews in header

--- Wanikani/Lessons & Reviews In Header/userscript.js
+++ Wanikani/Lessons & Reviews In Header/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani: Lessons & Reviews in header
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.2.4
 // @description  Puts the lessons and review counts back into the header
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(dashboard)?$/
@@ -12,7 +12,7 @@
     document.getElementsByTagName('head')[0].insertAdjacentHTML(
         'beforeEnd',
         `
-<style id="L&RHeader">
+<style id="LnRHeader">
     .navigation-shortcuts.hidden {display: flex; visibility: visible;}
     .lessons-and-reviews {display: none;}
     .extra-study {grid-row: 1/3 !important;}
