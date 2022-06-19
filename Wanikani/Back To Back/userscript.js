72f72337c329005eca4473223e8aee633add5138
Kumirei <linusson.sixten@gmail.com>
2022-06-19 12:04:59 -0400

    Back To Back: Fix choosing the question when Double Check undoes reviews

--- Wanikani/Back To Back/userscript.js
+++ Wanikani/Back To Back/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wanikani: Back to back
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @description  Makes reading and meaning appear back to back in reviews and lessons
 // @author       Kumirei
 // @include      /^https://(www|preview).wanikani.com/(lesson|review|extra_study)/session/
@@ -95,11 +95,13 @@
                 const new_active_queue = [item, ...active_queue.filter((i) => i !== item)]
                 $.jStorage.set(active_queue_key, new_active_queue)
                 // Set the question type
-                let question = 'meaning'
-                if (item.type !== 'Radical') {
+                let question = ($.jStorage.get < 'meaning') | ('reading' > (question_type_key, 'meaning'))
+                if (item.type === 'Radical') question = 'meaning'
+                else {
                     const UID = (item.type == 'Kanji' ? 'k' : 'v') + item.id
                     const stats = $.jStorage.get(UID_prefix + UID, {})
-                    question = ['meaning', 'reading'][stats.mc ? 1 : stats.rc ? 0 : Math.floor(Math.random() * 2)]
+                    if (stats.mc) question = 'reading'
+                    else if (stats.rc) question = 'meaning'
                 }
                 $.jStorage.set(question_type_key, question)
                 // Pass the value to the original set function
