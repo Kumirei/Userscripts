d0b5345578abcaebf2ae12220d8c970040af2256
Kumirei <linusson.sixten@gmail.com>
2023-03-30 18:23:04 -0400

    Update wkQueue

--- Wanikani/Reorder Omega/reorder.user.ts
+++ Wanikani/Reorder Omega/reorder.user.ts
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Wanikani: Reorder Omega
 // @namespace    http://tampermonkey.net/
-// @version      1.3.24
+// @version      1.3.25
 // @description  Reorders n stuff
 // @author       Kumirei
 // @match        https://www.wanikani.com/*
 // @match        https://preview.wanikani.com/*
-// @require      https://greasyfork.org/scripts/462049-wanikani-queue-manipulator/code/WaniKani%20Queue%20Manipulator.user.js?version=1166906
+// @require      https://greasyfork.org/scripts/462049-wanikani-queue-manipulator/code/WaniKani%20Queue%20Manipulator.user.js?version=1168389
 // @grant        none
 // @run-at       document-idle
 // @license      MIT
