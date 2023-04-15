ac291ecf8aeb47447f5de326d17a8b349d55f8b1
Kumirei <linusson.sixten@gmail.com>
2023-04-15 16:00:11 -0400

    Fast Vocab Breakdown: fix after WK change

--- Wanikani/Fast Vocab Breakdown/userscript.js
+++ Wanikani/Fast Vocab Breakdown/userscript.js
@@ -1,42 +1,59 @@
 // ==UserScript==
 // @name         Wanikani: Fast Vocab Breakdown
 // @namespace    http://tampermonkey.net/
-// @version      1.1.1
+// @version      1.1.2
 // @description  Automatically displays the meanings of the kanji when you get a vocab item wrong
 // @author       Kumirei
 // @include      *wanikani.com/review/session
 // @grant        none
 // ==/UserScript==
 
-;(function ($, wkof) {
-    // Make sure WKOF is installed
+;(async function (wkof) {
     let script_name = 'Fast Vocab Breakdown'
     let script_id = 'fast_vocab_breakdown'
-    if (!wkof) {
-        if (
-            confirm(
-                script_name +
-                    ' requires Wanikani Open Framework.\nDo you want to be forwarded to the installation instructions?',
-            )
-        ) {
-            window.location.href =
-                'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549'
+    let items, elem, answer
+
+    confirm_wkof()
+    await init()
+    window.addEventListener(`turbo:render`, init)
+    window.addEventListener(`turbo:before-render`, before_render)
+    window.addEventListener(`didAnswerQuestion`, handle_answer)
+    window.addEventListener(`willShowNextQuestion`, hide_breakdown)
+
+    function before_render(event) {
+        const page = get_page()
+        const new_body = event.detail.newBody
+
+        if (page === 'reviews') {
+            install_element(new_body)
         }
-        return
     }
 
-    // Setup
-    let items, elem
-    let answer = document.querySelector('#answer-form fieldset')
-    wkof.include('ItemData,Settings,Menu')
-    wkof.ready('ItemData,Settings,Menu').then(load_settings).then(install_menu).then(fetch_items)
-    install_element()
+    function get_page() {
+        const url = window.location.pathname
+        if (/\/subjects\/review/.test(url)) return 'reviews'
+    }
 
-    // Initialize
-    $.jStorage.listenKeyChange('questionCount', handle_answer)
-    $.jStorage.listenKeyChange('currentItem', () => {
-        elem.classList.add('hidden')
-    })
+    async function init() {
+        wkof.include('ItemData,Settings,Menu')
+        await wkof.ready('ItemData,Settings,Menu').then(load_settings).then(install_menu).then(fetch_items)
+        install_element(document.body)
+    }
+
+    function confirm_wkof() {
+        if (!wkof) {
+            if (
+                confirm(
+                    script_name +
+                        ' requires Wanikani Open Framework.\nDo you want to be forwarded to the installation instructions?',
+                )
+            ) {
+                window.location.href =
+                    'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549'
+            }
+            return
+        }
+    }
 
     // Populates the item database with items indexed by subject id
     async function fetch_items() {
@@ -44,29 +61,38 @@
     }
 
     // Installs the info element and sets up the page
-    function install_element() {
+    function install_element(body) {
         install_css()
-        document
-            .querySelector('#question #character')
-            .insertAdjacentHTML('beforeend', '<div id="' + script_id + '" class="hidden"></div>')
-        elem = document.getElementById(script_id)
+        body.querySelector('.character-header__content').insertAdjacentHTML(
+            'beforeend',
+            '<div id="' + script_id + '" class="hidden"></div>',
+        )
+        elem = body.querySelector(`#${script_id}`)
     }
 
     // Decides what to do when a new answer is submitted
-    function handle_answer() {
+    function handle_answer(event) {
+        if (event.detail.subjectWithStats.subject.type !== 'Vocabulary') return
         const settings = wkof.settings.fast_vocab_breakdown
-        const item = $.jStorage.get('currentItem')
-        let show = false
-        const is_quest = settings.question == 'both' || settings.question == $.jStorage.get('questionType', 'reading')
-        const is_ans = settings.answer == 'both' || answer.classList.contains(settings.answer)
-        if (item.voc && is_quest && is_ans) insert_info(item)
+        const questionType = event.detail.questionType
+        const passed = event.detail.results.passed
+
+        const is_quest = settings.question == 'both' || settings.question == questionType
+        const is_ans = settings.answer == 'both' || (settings.answer === 'correct') === passed
+
+        console.log({ settings, questionType, passed, is_quest, is_ans })
+        if (is_quest && is_ans) insert_info(event.detail.subjectWithStats.subject.id)
         else elem.classList.add('hidden')
     }
 
+    function hide_breakdown() {
+        elem.classList.add('hidden')
+    }
+
     // Finds and inserts the kanji info when an incorrect answer is submitted
-    function insert_info(item) {
+    function insert_info(itemId) {
         let meanings = []
-        items[item.id].data.component_subject_ids.forEach((id) => {
+        items[itemId].data.component_subject_ids.forEach((id) => {
             items[id].data.meanings.forEach((meaning) => {
                 if (meaning.primary) meanings.push(meaning.meaning)
             })
@@ -83,10 +109,9 @@
             `<style id="${script_id}-css">
 #${script_id} {
     position: absolute;
-    line-height: normal;
     font-size: 18px;
-    width: 100%;
-    margin-top: -30px;
+    bottom: 0;
+    padding: 0.5rem;
 }
 #${script_id}.hidden {visibility: hidden;}
 .srs { top: -3em;}
@@ -144,4 +169,4 @@
         let dialog = new wkof.Settings(config)
         dialog.open()
     }
-})(window.jQuery, window.wkof)
+})(window.wkof)
