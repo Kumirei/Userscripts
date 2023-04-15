ddc95f2ab6c086ae562683c532b73c523d4f096f
Kumirei <linusson.sixten@gmail.com>
2023-04-15 15:30:45 -0400

    Review Queue Sizer: fix after WK update

--- Wanikani/Review Queue Sizer/userscript.js
+++ Wanikani/Review Queue Sizer/userscript.js
@@ -1,49 +1,57 @@
 // ==UserScript==
 // @name         Wanikani: Review Queue Sizer
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Lets you choose the size of your review session
 // @author       Kumirei
-// @include      /^https://(www|preview).wanikani.com/(lesson|review)/session$/
+// @match        https://www.wanikani.com/*
+// @match        https://preview.wanikani.com/*
+// @require      https://greasyfork.org/scripts/462049-wanikani-queue-manipulator/code/WaniKani%20Queue%20Manipulator.user.js?version=1172719
 // @grant        none
 // ==/UserScript==
 
-;(function (wkof, $) {
+;(async function (wkof, $) {
     let script_name = 'Wanikani: Review Queue Sizer'
     let script_title = 'Review Queue Sizer'
     let script_id = 'review_queue_sizer'
+    let settings
 
-    // Make sure WKOF is installed
-    if (!wkof) {
-        let response = confirm(
-            script_name +
-                ' requires WaniKani Open Framework.\n Click "OK" to be forwarded to installation instructions.',
-        )
-        if (response) {
-            window.location.href =
-                'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549'
+    confirm_wkof()
+    await init()
+    window.addEventListener(`turbo:render`, init)
+    window.wkQueue.addFilter((queue) => {
+        if (!settings?.qsize) return queue
+        return queue.slice(0, settings.qsize)
+    })
+
+    function confirm_wkof() {
+        // Make sure WKOF is installed
+        if (!wkof) {
+            let response = confirm(
+                script_name +
+                    ' requires WaniKani Open Framework.\n Click "OK" to be forwarded to installation instructions.',
+            )
+            if (response) {
+                window.location.href =
+                    'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549'
+            }
+            return
         }
+    }
+
+    async function init() {
+        wkof.include('Menu,Settings,Jquery')
+        await wkof.ready('Menu,Settings,Jquery')
+        await load_settings()
+        install_menu()
         return
     }
 
-    wkof.include('Menu,Settings,ItemData')
-    wkof.ready('Menu,Settings').then(load_settings).then(install_menu).then(start)
-
-    function start() {
-        // Run once items have loaded, and only once
-        let stop = false
-        $.jStorage.listenKeyChange('reviewQueue', () => {
-            if (!stop) {
-                stop = true
-                run()
-            }
-        })
-    }
-
     // Load WKOF settings
-    function load_settings() {
+    async function load_settings() {
         let defaults = { qsize: 0 }
-        return wkof.Settings.load(script_id, defaults)
+        settings = await wkof.Settings.load(script_id, defaults)
+        return
     }
 
     // Installs the options button in the menu
@@ -61,9 +69,7 @@
         var config = {
             script_id: script_id,
             title: script_title,
-            pre_open: settings_pre_open,
             on_save: settings_on_save,
-            on_close: run,
             content: {
                 qsize: {
                     type: 'number',
@@ -78,40 +84,6 @@
     }
 
     function settings_on_save(settings) {
-        run()
+        window.wkQueue.refresh()
     }
-
-    function settings_pre_open(settings) {
-        const target = settings[0].querySelector('#review_queue_sizer_qsize')
-        target.addEventListener('keydown', (e) => {
-            if (e.target.nodeName == 'INPUT' && e.keyCode == 8) {
-                e.target.value = e.target.value.slice(0, -1)
-                fire_event(target, 'change')
-            }
-        })
-    }
-
-    // Retrieves the current review queue
-    function get_queue() {
-        const items = [...($.jStorage.get('activeQueue') ?? []), ...$.jStorage.get('reviewQueue')]
-        return items
-    }
-
-    // Create new queue
-    function run() {
-        let items = get_queue()
-        const limit = wkof.settings[script_id].qsize || items.length
-        const done = $.jStorage.get('completedCount')
-        $('#bar').width((done / limit) * 100 + '%')
-        const truncated = items.slice(0, limit - done)
-        $.jStorage.set('reviewQueue', truncated.slice(10))
-        $.jStorage.set('activeQueue', truncated.slice(0, 10))
-        $.jStorage.set('currentItem', truncated[0])
-    }
-
-    function fire_event(elem, event) {
-        let e = document.createEvent('HTMLEvents')
-        e.initEvent(event, true, true) // Type, bubbling, cancelable
-        return !elem.dispatchEvent(e)
-    }
-})(window.wkof, window.jQuery)
+})(window.wkof)
