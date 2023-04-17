7f5bf614f247cc1ccc5895c1df07e1f06c2e1a46
Kumirei <linusson.sixten@gmail.com>
2023-04-16 21:11:36 -0400

    New script!

---
+++ Wanikani/Detailed SRS Popup/userscript.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         Wanikani: Detailed SRS Popups
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Changes apprentice and guru popups to say their respective numbers. E.G. Apprentice 1 or Guru 2
+// @author       Kumirei
+// @match        https://www.wanikani.com/*
+// @match        https://preview.wanikani.com/*
+// @require      https://greasyfork.org/scripts/462049-wanikani-queue-manipulator/code/WaniKani%20Queue%20Manipulator.user.js?version=1172719
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+;(function () {
+    const srs_stages = [
+        'Initiate',
+        'Apprentice 1',
+        'Apprentice 2',
+        'Apprentice 3',
+        'Apprentice 4',
+        'Guru 1',
+        'Guru 2',
+        'Master',
+        'Enlighten',
+        'Burn',
+    ]
+
+    let item_data = {}
+    wkQueue.on('review').addPostprocessing(update_item_srs)
+    window.addEventListener('didAnswerQuestion', update_stats)
+    window.addEventListener('didChangeSRS', change_srs_name)
+
+    // Get latest item SRS info
+    function update_item_srs(queue) {
+        for (let item of queue) {
+            item_data[item.id] = { srs: item.srs, stats: {} }
+        }
+    }
+
+    // Keep track of the stats of the most recent item
+    let last_item
+    function update_stats(event) {
+        const id = event.detail.subjectWithStats.subject.id
+        item_data[id].stats = event.detail.subjectWithStats.stats
+
+        last_item = item_data[id]
+    }
+
+    // Change the name in the popup
+    function change_srs_name(event) {
+        const new_srs = calculate_resulting_srs(last_item)
+        event.detail.newLevelText = srs_stages[new_srs]
+    }
+
+    // Calculate resulting SRS based on current SRS level and number of mistakes made
+    function calculate_resulting_srs(item) {
+        const incorrect = item.stats.meaning.incorrect + item.stats.reading.incorrect
+        if (incorrect === 0) return item.srs + 1
+
+        const incorrect_adjustment = Math.ceil(incorrect / 2)
+        const penalty = item.srs >= 5 ? 2 : 1
+        const new_srs = item.srs - incorrect_adjustment * penalty
+
+        return new_srs < 1 ? 1 : new_srs
+    }
+})()
