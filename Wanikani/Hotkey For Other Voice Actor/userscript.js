7fbef8ed9de75f81715e688f91633a985336a96a
Kumirei <linusson.sixten@gmail.com>
2023-04-29 12:39:55 -0400

    VA Hotkey: fix after WK changes

--- Wanikani/Hotkey For Other Voice Actor/userscript.js
+++ Wanikani/Hotkey For Other Voice Actor/userscript.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Wanikani: Hotkey for other voice actor
-// @description Binds the I key to play the audio from the other vocie actor
-// @include     *wanikani.com/review/session*
-// @include     *wanikani.com/lesson/session*
-// @version     1.1.5
+// @description Binds the I key to play the audio from the other voice actor
+// @match       https://www.wanikani.com/*
+// @match       https://preview.wanikani.com/*
+// @version     1.1.6
 // @author      Kumirei
 // @license     MIT; http://opensource.org/licenses/MIT
 // @run-at      document-end
@@ -13,25 +13,58 @@
 
 ;(function () {
     const key = 'i'
-    $(document).on('keydown', (event) => {
-        let audio_disabled = $('#option-audio')[0].className == 'disabled'
-        if ($('#lessons').length && $('#lessons > div > header')[0].className != 'quiz') audio_disabled = false
-        let tag = event.target.tagName
-        if (tag != 'TEXTAREA' && tag != 'INPUT' && event.key == key && !audio_disabled) play_other_voice()
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key !== key) return
+
+        // Not a quiz page
+        const has_audio = !!document.querySelector('.additional-content__item--audio')
+        if (!has_audio) return
+
+        // Not ready to play audio
+        let audio_disabled = !!document
+            .querySelector('.additional-content__item--audio')
+            .classList.contains('additional-content__item--disabled')
+        if (audio_disabled) return
+
+        // Currently typing
+        if (/textarea|input/i.test(event.target.tagName)) return
+
+        play_other_voice()
     })
 
     function play_other_voice() {
-        let audio = new Audio()
-        let audios = $.jStorage.get('currentItem').aud
-        if ($('#lessons').length) {
-            audios = $.jStorage.get('l/currentLesson').aud
-            if ($.jStorage.get('l/quizActive')) audios = $.jStorage.get('l/currentQuizItem').aud
-        }
-        let vaAudio = audios.filter((a) => a.voice_actor_id == (window.WaniKani.default_voice_actor_id % 2) + 1)
-        console.log(vaAudio);
-        vaAudio.forEach((a) =>
-            audio.insertAdjacentHTML('beforeend', `<source src="${a.url}" type+"${a.content_type}">`),
+        // Get URL of current VA (if any)
+        const quizAudioSource = document.querySelector('audio.quiz-audio__audio source:first-child')?.src
+        if (!quizAudioSource) return // Don't do anything if no audio
+
+        // Parse items, find correct item, find entered reading, find entered pronunciation, pick different pronunciation of same reading
+        const items = JSON.parse(
+            document.querySelector('#quiz-queue script[data-quiz-queue-target="subjects"]').textContent,
         )
-        audio.play()
+        for (let item of items || []) {
+            for (let reading of item.readings || []) {
+                let isCorrectReading = false
+                let currentPronunciation = null
+                pronunciations: for (let pronunciation of reading.pronunciations || []) {
+                    for (let source of pronunciation.sources || []) {
+                        if (source.url === quizAudioSource) {
+                            isCorrectReading = true
+                            currentPronunciation = pronunciation
+                            break pronunciations
+                        }
+                    }
+                }
+                if (!isCorrectReading) continue
+
+                const pronunciation = reading.pronunciations.find(
+                    (pronunciation) => pronunciation !== currentPronunciation,
+                )
+                const source = pronunciation.sources[0].url
+                const audio = new Audio(source)
+                audio.play()
+                return
+            }
+        }
     }
 })()
